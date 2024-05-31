b11cc4594ac9b07c6a92fd2e222cf1263b3e2157
rakibmiah99 <rakibmiah120199@gmail.com>
2024-05-31 21:16:59 +0600

    ee

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -16,7 +16,7 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withExceptions(function (Exceptions $exceptions) {
 
         $exceptions->render(function (Exception $exception){
-            dd($exception->getMessage());
+            dd($exception->getMessage()) ;
         });
 
 
